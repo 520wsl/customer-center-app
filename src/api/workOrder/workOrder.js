56b119bc7570ae12643082da7c45f72288b10c13
2355456062@qq.com <2355456062@qq.com>
2018-11-22 09:38:23 +0800

    fix:新增api文件

---
+++ src/api/workOrder/workOrder.js
@@ -0,0 +1,11 @@
+import api from "@/libs/api.request";
+
+const baseUrl = "/work-order";
+
+function post(url, params) {
+  return api.post(baseUrl + url, params);
+}
+
+function get(url, params) {
+  return api.get(baseUrl + url, params);
+}
