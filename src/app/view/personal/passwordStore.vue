c5d8c9695abac4e8c768f04393a180046252bcf5
jack_blue <2355394133@qq.com>
2018-12-02 20:22:56 +0800

    1

--- src/app/view/personal/passwordStore.vue
+++ src/app/view/personal/passwordStore.vue
@@ -82,7 +82,7 @@ export default {
 
   },
   async created() {
-    alert(JSON.stringify(this.$store.User))
+    alert(JSON.stringify(this.$store.state))
     this.$parent.$parent.setTitle("店铺账号密码");
     this.companySixiId = this.$route.query.companySixiId || '';
     const res = await selectCompanyAndMobile();
