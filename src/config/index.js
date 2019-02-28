e351a1d74ccef1f0d80f08a7f2f89bd5156a84c1
2355456062@qq.com <2355456062@qq.com>
2019-02-28 19:00:53 +0800

    fix(登录模块): 修改配置

--- src/config/index.js
+++ src/config/index.js
@@ -13,7 +13,8 @@ export default {
     CDN: "http://custom-center.oss-cn-hangzhou.aliyuncs.com/customerCenter/image",
     AUDIOCDN: "http://wechat-base-images.oss-cn-hangzhou.aliyuncs.com/",
     storeagewxUserInfoKey: "wxUserInfo",
-    toLoginUrl: "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx3c0c1aef00b3d175&redirect_uri=http://workapp.sixi.com/personal/index&response_type=code&scope=snsapi_userinfo&state=weChat&connect_redirect=1#wechat_redirect",
+    toLoginUrl: "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx3c0c1aef00b3d175&redirect_uri=https%3A%2F%2Foauth.sixi.com%2Fgzh%2Fcallback&response_type=code&scope=snsapi_base&state=Y2xpZW50X2lkPXdvcmtvcmRlciZyZWRpcmVjdF91cmk9aHR0cCUzQSUyRiUyRndvcmthcHAuc2l4aS5jb20lMkZwZXJzb25hbCUyRmluZGV4JTNGcGFyJTNEY0dGblpVNWhiV1VsTTBSd1pYSnpiMjVoYkZObGNuWnBaUSUzRCUzRCZzdGF0ZT1zc28=#wechat_redirect",
+    // toLoginUrl: "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx3c0c1aef00b3d175&redirect_uri=http://workapp.sixi.com/personal/index&response_type=code&scope=snsapi_userinfo&state=weChat&connect_redirect=1#wechat_redirect",
     ssoConfig: {
         login: {
             clientId: 'workorder',
